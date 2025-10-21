5e1d1f84ff24746deb04592d213566d43eece63d
Romain <romain.braud@e-artsup.net>
2025-10-21 14:47:02 +0100

    Juste pour récupéré

---
+++ SkillPassif.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Skill.h"
+class SkillPassif : public Skill
+{
+protected:
+
+	//double AttackStat 
+
+
+	double BuffPassive() {
+
+	}
+
+	double NerfPassive() {
+
+	}
+
+	
+};
+
+
